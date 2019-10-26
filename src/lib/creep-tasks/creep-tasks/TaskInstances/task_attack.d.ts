9ed0980ee0152194c395a674f90f7e6966e5ffea
zkl2333 <i@zkl2333.com>
2019-10-26 20:59:53 +0800

    lib

---
+++ src/lib/creep-tasks/creep-tasks/TaskInstances/task_attack.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="screeps" />
+import { Task } from '../Task';
+export declare type attackTargetType = Creep | Structure;
+export declare class TaskAttack extends Task {
+    static taskName: string;
+    target: attackTargetType;
+    constructor(target: attackTargetType, options?: TaskOptions);
+    isValidTask(): boolean;
+    isValidTarget(): boolean;
+    work(): number;
+}
